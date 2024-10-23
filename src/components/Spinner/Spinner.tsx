e639ad90d7dba7f595cb637410db2512c1f68e40
pervin <pervinagayev28@gmail.com>
2024-10-23 09:51:49 +0400

    read for pre production 3

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Spinner.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
